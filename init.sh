4e873c572a532b712a86f125aae506749eb880fa
William Kamau <william.kamuyu@gapstack.com>
2024-01-09 11:45:47 +0300

    Updated values

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(change-me.org www.change-me.org)
+domains=(api-farm.duckdns.org www.api-farm.duckdns.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
