3f1e2eae53a61760056e124040f751cdf75c406f
William Kamau <william.kamuyu@gapstack.com>
2024-01-09 13:14:17 +0300

    Updated values

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ fi
 domains=(api-farm.duckdns.org www.api-farm.duckdns.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="kamuyu.will@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
